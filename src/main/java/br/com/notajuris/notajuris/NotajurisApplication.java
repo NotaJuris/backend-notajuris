958ab0fbf7eb4324363dc18d92268c42ea11a2a3
calixtollucas <lucaslucenacalixto1@gmail.com>
2024-09-17 08:28:18 -0300

    initial commit

---
+++ src/main/java/br/com/notajuris/notajuris/NotajurisApplication.java
@@ -0,0 +1,13 @@
+package br.com.notajuris.notajuris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotajurisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotajurisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/notajuris/notajuris/NotajurisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.notajuris.notajuris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotajurisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
