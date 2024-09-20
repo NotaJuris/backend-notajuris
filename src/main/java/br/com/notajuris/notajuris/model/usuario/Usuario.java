5aea198430ca5ae80c41a8fe4fab544e38d1c8dc
calixtollucas <lucaslucenacalixto1@gmail.com>
2024-09-19 21:16:44 -0300

    add-jwt-service

--- src/main/java/br/com/notajuris/notajuris/model/usuario/Usuario.java
+++ src/main/java/br/com/notajuris/notajuris/model/usuario/Usuario.java
@@ -17,7 +17,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "usuario")
 public class Usuario implements UserDetails{
