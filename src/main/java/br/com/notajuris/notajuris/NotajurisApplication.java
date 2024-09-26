9758e285ae394f5a1a103e912a9aa44b6f739742
calixtollucas <lucaslucenacalixto1@gmail.com>
2024-09-26 19:58:46 -0300

    refactor: deploy prep

--- src/main/java/br/com/notajuris/notajuris/NotajurisApplication.java
+++ src/main/java/br/com/notajuris/notajuris/NotajurisApplication.java
@@ -6,6 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NotajurisApplication {
 
+	public String MYSQLHOST = System.getenv("MYSQLHOST");
+	public String MYSQLPORT = System.getenv("MYSQLPORT");
+	public String MYSQLDATABASE = System.getenv("MYSQLDATABASE");
+	public String MYSQLUSER = System.getenv("MYSQLUSER");
+	public String MYSQLPASSWORD = System.getenv("MYSQLPASSWORD");
+	public String JWTKEY = System.getenv("JWTKEY");
+	public String REDISHOST = System.getenv("REDISHOST");
+	public String REDISPORT = System.getenv("REDISPORT");
+	public String REDISUSER = System.getenv("REDISUSER");
+	public String REDISPASSWORD = System.getenv("REDISPASSWORD");
+	public String SPRINGPROFILE = System.getenv("SPRINGPROFILE");
+
 	public static void main(String[] args) {
 		SpringApplication.run(NotajurisApplication.class, args);
 	}
